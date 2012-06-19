226a15a9a4747b4c8f32dda711a6859640bd3817
Kitipong Yodsak <id_kiti_im@hotmail.com>
2012-06-19 15:02:35 +0700

    เพิ่มโค้ด

--- src/th/ac/nu/student/u54341581/compro/ch01/HelloWorld.java
+++ src/th/ac/nu/student/u54341581/compro/ch01/HelloWorld.java
@@ -15,6 +15,8 @@ public class HelloWorld {
 */
 public static void main(String[] args) {
 // TODO code application logic here
-System.out.println("Hello,World!");
+System.out.println("Hello World!");
+System.out.println("Programming is fun.");
+
 }
 }
